1d6c5c451b95f436d60a8a8469b7e4aaecc94a49
李华 <2088595113@qq.com>
2019-06-25 22:10:27 +0800

    Initial commit

---
+++ demoModule/src/com/itxigua/demo/Demo.java
@@ -0,0 +1,13 @@
+package com.itxigua.demo;
+
+/**
+ * @author cymstart
+ * @create 2019-06-25 20:51
+ */
+public class Demo {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("中国梦， 世界梦");
+        }
+    }
+}
